fabc3ac6579de450e6c6ab13e879d3dda3795ea8
Josh <36666883+Hibyehello@users.noreply.github.com>
2023-02-19 14:25:22 -0600

    Update build script to take region value

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 if [ -z "$1" ]
 then
-	echo "No input source file!"
+	echo
+	echo "Usage: ./build.sh <input-file>.S <region>"
+	echo
 	exit
 fi	
 
 echo
-if $DEVKITPPC/bin/powerpc-eabi-gcc -shared -nostdlib -Wl,--oformat binary -Wa,-mregnames $1 -o out.o; then
+if $DEVKITPPC/bin/powerpc-eabi-gcc -shared -nostdlib -Wl,--oformat binary -Wa,-mregnames -Dregion=\"$2\" $1 -o out.o; then
 	hexdump -e '4/1 "%02x" " " 4/1 "%02x" "\n"' out.o 
 fi
 echo
